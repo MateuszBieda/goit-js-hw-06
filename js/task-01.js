d08d6ff2cf3da8f6daf3549089599e417398996b
MateuszBieda <mateusbieda@gmail.com>
2023-02-21 22:48:05 +0100

    tasks_01and_02update

---
+++ js/task-01.js
@@ -0,0 +1,19 @@
+//1. 
+//version 1
+const categories = document.querySelector('#categories');
+categories.children.length;
+//console.log('Number of categories: ',categories.children.length);
+
+//version 2
+const items = document.querySelectorAll('li.item');
+[...items].length;
+//console.log('Number of categories: ',[...items].length);
+
+//2
+const list = document.querySelectorAll('.item >h2');
+//console.log (categories.children.length);
+//console.log([...list]);
+
+for (i=0; i<categories.children.length;i++){
+  console.log('Category: ', list[i].innerText);
+  console.log('Elements: ',list[i].nextElementSibling.children.length)}
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+for (i=0;i<ingredients.length;i++){
+ const list = document.createElement("li");
+  list.classList.add('item');
+ list.textContent = ingredients[i]; 
+ ingredientsList.appendChild(list);
+  
+}
+const finalList = document.querySelector('ul#ingredients');
+//console.log(finalList);
