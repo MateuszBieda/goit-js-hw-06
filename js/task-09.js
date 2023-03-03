a6e6084db5a901c5cc401cd8d0125b6254d00ba9
MateuszBieda <mateusbieda@gmail.com>
2023-03-03 20:03:17 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+  
+
+const color = document.querySelector('.color');
+const body = document.querySelector("body");
+const changeColor = document.querySelector(".change-color");
+changeColor.addEventListener("click", (event)=>{
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor= randomColor;
+  color.textContent=randomColor;
+});
