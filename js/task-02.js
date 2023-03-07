4e00d00088aec7ac5f685e550284d12b1784a22d
MateuszBieda <mateusbieda@gmail.com>
2023-03-07 19:18:28 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -9,10 +9,22 @@ const ingredients = [
 
 const ingredientsList = document.querySelector('#ingredients');
 
-for (let i=0;i<ingredients.length;i++){
- const list = document.createElement("li");
-  list.classList.add('item');
- list.textContent = ingredients[i]; 
- ingredientsList.appendChild(list);
-  
+/* const elements = ingredients.map((ingredient) => 
+  {const list = document.createElement("li");
+  list.textContent= ingredient;
+  list.classList.add("item");
+  return list;
+});
+ingredientsList.append(...elements);
+ */
+const elements =[];
+
+for (let i=0; i<ingredients.length; i++){
+ // const element = ingredients[i];
+  const list = document.createElement("li");
+  list.textContent= ingredients[i];
+  list.classList.add("item");
+  elements.push(list);
+
 }
+ingredientsList.append(...elements);
